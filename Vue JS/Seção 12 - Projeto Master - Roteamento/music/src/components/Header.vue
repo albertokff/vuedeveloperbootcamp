9e91d64b882b1c8b7b3d2a1ece44b930c9e9aeba
DiogoAlbertoRK <kirchoffalberto@gmail.com>
2024-03-28 14:34:43 -0300

    Seção 12 - Projeto Master - Routing finalizada

--- Vue JS/Seção 12 - Projeto Master - Roteamento/music/src/components/Header.vue
+++ Vue JS/Seção 12 - Projeto Master - Roteamento/music/src/components/Header.vue
@@ -3,10 +3,18 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Dev's App Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Dev's App Music</router-link
+      >
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
+          <li>
+            <router-link class="px-2 text-white" to="/about">Sobre</router-link>
+          </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -15,10 +23,12 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Gerenciar</a>
+              <router-link class="px-2 text-white" to="/manage">Gerenciar</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Sair</a>
+              <router-link class="px-2 text-white" to="/exit" @click.prevent="signOut"
+                >Sair</router-link
+              >
             </li>
           </template>
         </ul>
@@ -40,6 +50,13 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
+    },
+    signOut() {
+      this.userStore.signOut()
+
+      if (this.$router.meta.requiresAuth) {
+        this.$router.push({ name: 'home' })
+      }
     }
   }
 }
